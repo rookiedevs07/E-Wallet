41c7357795b4021372af2a5649a19b59ffefe2af
onkar <onkar.salvi07@gmail.com>
2024-10-15 22:52:13 +0530

    pr for test

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -3,5 +3,10 @@ import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      merchant test merchant test merchant test merchant test merchant test
+      merchant test
+    </div>
+  );
 }
